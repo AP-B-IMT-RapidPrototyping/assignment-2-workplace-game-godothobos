5189942a48f71262de092fb12d95221f47561af3
CasuallySleepy <145465388+CasuallySleepy@users.noreply.github.com>
2026-03-24 13:58:21 +0100

    moved the arrow script to correct folder, changed arrow position and color, fixed npc clipping through map, fixed npc floating

--- StressLibrarian/Scripts/CharacterScripts/DirectionArrow.cs
+++ StressLibrarian/Scripts/CharacterScripts/DirectionArrow.cs
@@ -4,16 +4,21 @@ using System;
 public partial class DirectionArrow : Node3D
 {
     public Node3D Target;
+    [Export] private AnimationPlayer _fadeAnimation;
 
     public override void _Process(double delta)
     {
         if (Target == null)
         {
+            _fadeAnimation.Stop();
             Visible = false;
             return;
         }
 
         Visible = true;
+
+        _fadeAnimation.Play("fade");
+
 
         Vector3 from = GlobalTransform.Origin;
         Vector3 to = Target.GlobalTransform.Origin;
